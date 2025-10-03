c609f9bbc5b54fc82526872947148c8f605998ab
CarlosGiovaneNN <carlosgiovane1289@gmail.com>
2025-10-03 18:19:40 -0300

    feat - adicionando o metodo diferencas divididas no menu

--- main.py
+++ main.py
@@ -7,12 +7,15 @@ from linear_systems.gauss_method.solver import run_gauss
 from linear_systems.jordan_method.solver import run_jordan
 from linear_systems.gauss_seidel_method.solver import run_gauss_seidel
 
+from interpolation.divided_differences.solver import run_divided_differences
+
 while True:
     print("\nSolvers")
 
     print("1. Root Finding")
     print("2. Linear System")
-    print("3. Exit")
+    print("3. Divided Differences")
+    print("4. Exit")
 
     choice = int(input("Enter your choice (1-3): "))
 
@@ -58,6 +61,19 @@ while True:
             continue
 
     elif choice == 3:
+        print("\nInterpolation Methods:")
+
+        print("1. Divided Differences")
+
+        choice = int(input("Enter your choice: "))
+
+        if choice == 1:
+            run_divided_differences()
+        else:
+            print("Invalid choice")
+            continue
+
+    elif choice == 4:
         break
 
     else:
